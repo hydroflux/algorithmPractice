b84a29c4cb8d92f5e42b7295eee7cd0988ef99b8
Jack Hubert <jphubert93@gmail.com>
2021-07-07 14:16:47 -0600

    created a secondary function to solve the phone number array using a different method

--- createPhoneNumber.js
+++ createPhoneNumber.js
@@ -1,7 +1,20 @@
 // Write a function that accepts an array of 10 integers (between 0 and 9),
 // that returns a string of those numbers in the form of a phone number
 
-function createPhoneNumber(numberArray){
+
+// Convert to string, format using slice
+function createPhoneNumber( numberArray ){
     const numberString = numberArray.join("")
     return `(${numberString.slice(0,3)}) ${numberString.slice(3,6)}-${numberSting.slice(6,11)}`
+}
+
+// Create format, replace placeholders with numbers in array
+function formatPhoneNumber( numberArray ){
+    let format = `(xxx) xxx-xxxx`
+
+    for ( let i = 0; i < numbers.length; i++ ){
+        format = format.replace('x', numberArray[i])
+    }
+
+    return format
 }
