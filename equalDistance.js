0cb334ee02aac8f4e8c1cd9893a7b6ed5a22da8f
Jack Hubert <jphubert93@gmail.com>
2021-07-07 15:03:19 -0600

    added test cases

--- equalDistance.js
+++ equalDistance.js
@@ -1,5 +1,3 @@
-import { countChar } from "./usefulFunctions/countChar";
-
 // CHALLENGE
 // Using an array of cardinal directions, determine if moving in each direction an equal distance
 // will return you to the same location, or if you will arrive in a new location
@@ -8,6 +6,18 @@ import { countChar } from "./usefulFunctions/countChar";
 // Array must contain exactly 10 elements
 
 // METHOD 1
+// Count number of occurrences of a character in an array
+function countChar(array, char){
+    let count = 0
+    for ( let i = 0; i < array.length; ++i ){
+        if ( array[i] == char ){
+        count ++
+        }
+    }
+
+    return count
+}
+
 // Determine if two elements of an array have the same count in that array
 function isEqualCount( array, a, b ){
     return countChar( array, a ) === countChar( array, b )
@@ -15,10 +25,21 @@ function isEqualCount( array, a, b ){
 
 function returnToStart( array ){
     if ( array.length === 10 ){
-        if ( isEqualCount( array, "N", "S") && isEqualCount( array, "E", "W" )){
+        if ( isEqualCount( array, "n", "s") && isEqualCount( array, "e", "w" )){
             return true
         }
     }
 
     return false
 }
+
+const arrayOne = ['n','s','n','s','n','s','n','s','n','s'] // true
+const arrayTwo = ['w','e','w','e','w','e','w','e','w','e','w','e'] // false
+const arrayThree = ['w'] // false
+const arrayFour = ['n','n','n','s','n','s','n','s','n','s'] // false
+
+// METHOD 1 TESTS
+console.log( returnToStart( arrayOne ) )
+console.log( returnToStart( arrayTwo ) )
+console.log( returnToStart( arrayThree ) )
+console.log( returnToStart( arrayFour ) )
