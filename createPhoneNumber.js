f14e03ab7391b119e322530a9e130b00f0e47b5c
Jack Hubert <jphubert93@gmail.com>
2021-07-07 14:23:46 -0600

    fixed syntax errors & printed solutions

--- createPhoneNumber.js
+++ createPhoneNumber.js
@@ -5,14 +5,14 @@
 // Convert to string, format using slice
 function createPhoneNumber( numberArray ){
     const numberString = numberArray.join("")
-    return `(${numberString.slice(0,3)}) ${numberString.slice(3,6)}-${numberSting.slice(6,11)}`
+    return `(${numberString.slice(0,3)}) ${numberString.slice(3,6)}-${numberString.slice(6,11)}`
 }
 
 // Create format, replace placeholders with numbers in array
 function formatPhoneNumber( numberArray ){
     let format = `(xxx) xxx-xxxx`
 
-    for ( let i = 0; i < numbers.length; i++ ){
+    for ( let i = 0; i < numberArray.length; i++ ){
         format = format.replace('x', numberArray[i])
     }
 
@@ -23,3 +23,8 @@ function formatPhoneNumber( numberArray ){
 function reducePhoneNumber( numberArray ){
     return numberArray.reduce( (placeholder, digit) => placeholder.replace("x", digit), "(xxx) xxx-xxxx")
 }
+
+const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 8]
+console.log( createPhoneNumber( numberArray ))
+console.log( formatPhoneNumber( numberArray ))
+console.log( reducePhoneNumber( numberArray ))
