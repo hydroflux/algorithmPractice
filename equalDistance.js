cb97f6a92e55a772c33f5d9e8d633dda7e13c351
Jack Hubert <jphubert93@gmail.com>
2021-07-07 15:08:51 -0600

    created a second method by determining distance based on an x or y axis value & using a switch/case statement

--- equalDistance.js
+++ equalDistance.js
@@ -5,7 +5,10 @@
 // RESTRAINT(S)
 // Array must contain exactly 10 elements
 
+// 
 // METHOD 1
+// 
+
 // Count number of occurrences of a character in an array
 function countChar(array, char){
     let count = 0
@@ -33,12 +36,42 @@ function returnToStart( array ){
     return false
 }
 
+// 
+// METHOD 2
+// 
+
+// Using a switch statement
+function returnToStart( array ){
+    let dx = 0
+    let dy = 0
+    let dt = array.length
+
+    for ( let i = 0; i < array.length; i++ ){
+        switch ( array[i] ) {
+            case 'n': dy--; break
+            case 's': dy++; break
+            case 'w': dx--; break
+            case 'e': dx++; break
+        }
+    }
+
+    return dt === 10 && dx === 0 && dy === 0
+}
+
+// SAMPLE SETS
 const arrayOne = ['n','s','n','s','n','s','n','s','n','s'] // true
 const arrayTwo = ['w','e','w','e','w','e','w','e','w','e','w','e'] // false
 const arrayThree = ['w'] // false
 const arrayFour = ['n','n','n','s','n','s','n','s','n','s'] // false
 
-// METHOD 1 TESTS
+// TEST CASES
+console.log( "Method 1 Tests:" )
+console.log( returnToStart( arrayOne ) )
+console.log( returnToStart( arrayTwo ) )
+console.log( returnToStart( arrayThree ) )
+console.log( returnToStart( arrayFour ) )
+
+console.log( "Method 2 Tests:" )
 console.log( returnToStart( arrayOne ) )
 console.log( returnToStart( arrayTwo ) )
 console.log( returnToStart( arrayThree ) )
