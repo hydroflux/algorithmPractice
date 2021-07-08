c335a4e0a4fbe06723005fbb2096e356e41d39b8
Jack Hubert <jphubert93@gmail.com>
2021-07-08 12:36:39 -0600

    added tests to replaceWithAlphabetPosition

--- replaceWithAlphabetPosition.js
+++ replaceWithAlphabetPosition.js
@@ -10,3 +10,15 @@ function alphabetPosition(text) {
   
     return convertedLetters.join(" ");
   }
+
+
+// SAMPLE DATA
+string1 = "The sunset sets at twelve o' clock."
+result1 = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+
+string2 = "The narwhal bacons at midnight."
+result2 = "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"
+
+// TESTS
+console.log( alphabetPosition(string1) === result1)
+console.log( alphabetPosition(string2) === result2)
