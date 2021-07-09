39804ec5471d89a104730b11c2eddb9612ce3748
Jack Hubert <jphubert93@gmail.com>
2021-07-09 12:26:44 -0600

    set up the persistance challenge

---
+++ challenges/persistance.js
@@ -0,0 +1,3 @@
+// CHALLENGE
+// Write a function that takes in a positive parameter & returns its multiplicative persistance,
+// which is the number of times you must multiply the digits until you reach a single digit
