5c448d9521bcd043de4e15a229557cec2750691c
Jack Hubert <jphubert93@gmail.com>
2021-07-09 15:10:55 -0600

    created persistance function, resulting in the count of the number of times multiplied

--- challenges/persistance.js
+++ challenges/persistance.js
@@ -1,3 +1,16 @@
 // CHALLENGE
 // Write a function that takes in a positive parameter & returns its multiplicative persistance,
 // which is the number of times you must multiply the digits until you reach a single digit
+
+function persistence(num) {
+    const stringArray = num => num.toString().split('')
+    const reducer = ( product, value ) => product * value
+    let count = 0
+    
+    while ( stringArray(num).length > 1){
+      num = stringArray(num).reduce( reducer )
+      count ++
+    }
+  
+    return count
+}
