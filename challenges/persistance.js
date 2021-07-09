ed82787a6087f0823f4b6eeaa594d1cd62f6839d
Jack Hubert <jphubert93@gmail.com>
2021-07-09 15:11:07 -0600

    set up tests for persistance

--- challenges/persistance.js
+++ challenges/persistance.js
@@ -14,3 +14,9 @@ function persistence(num) {
   
     return count
 }
+
+// TESTS
+console.log( persistence(39) === 3 )
+console.log( persistence(4) === 0 )
+console.log( persistence(25) === 2 )
+console.log( persistence(999) === 4 )
