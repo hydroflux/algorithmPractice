47dfff76b581ca3b5743a0de17af9e3783c326d6
Jack Hubert <jphubert93@gmail.com>
2021-07-14 10:28:42 -0600

    added tests for the getMiddle algorithm

--- challenges/middleCharacter.js
+++ challenges/middleCharacter.js
@@ -4,3 +4,15 @@
 // if the word's length is even, return the middle 2 characters
 
 // METHOD 1
+function getMiddle(s)
+{
+  return s.length % 2 == 0
+  ? s.slice( s.length/2 - 1 , s.length / 2 + 1)
+  : s[Math.floor(s.length/2)]
+}
+
+// TESTS
+console.log(getMiddle("test") === "es")
+console.log(getMiddle("testing") === "t")
+console.log(getMiddle("middle") === "dd")
+console.log(getMiddle("A") === "A")
