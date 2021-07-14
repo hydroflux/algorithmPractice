21eca4d73388ed220e95c3f82b38e72c186d085d
Jack Hubert <jphubert93@gmail.com>
2021-07-14 10:27:14 -0600

    created a middleCharacter challenge, & created a function to find the middle character

---
+++ challenges/middleCharacter.js
@@ -0,0 +1,6 @@
+// CHALLENGE
+// Given a word, return the middle character of the word
+// If the word's length is odd, return the middle character;
+// if the word's length is even, return the middle 2 characters
+
+// METHOD 1
