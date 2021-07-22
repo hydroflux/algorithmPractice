c1d214483aec04088506fc34ac2b96c61f9bbd7a
Jack Hubert <jphubert93@gmail.com>
2021-07-22 09:35:57 -0600

    set up the 'number' functions for calculatingWithFunctions

--- challenges/calculatingWithFunctions.js
+++ challenges/calculatingWithFunctions.js
@@ -9,3 +9,13 @@
 // Division should be integer division. For example, this should return 2, not 2.666666...:
 
 // METHOD 1
+const zero = operator => operator ? operator(0) : 0
+const one = operator => operator ? operator(1) : 1
+const two = operator => operator ? operator(2) : 2
+const three = operator => operator ? operator(3) : 3
+const four = operator => operator ? operator(4) : 4
+const five = operator => operator ? operator(5) : 5
+const six = operator => operator ? operator(6) : 6
+const seven = operator => operator ? operator(7) : 7
+const eight = operator => operator ? operator(8) : 8
+const nine = operator => operator ? operator(9) : 9
