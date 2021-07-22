c0682069ac81ef91cf7682167574a301a6ea7c00
Jack Hubert <jphubert93@gmail.com>
2021-07-22 09:10:26 -0600

    created a 'stringEndsWith' script & a method to determine if the first argument ends with the second argument

---
+++ challenges/stringEndsWith.js
@@ -0,0 +1,5 @@
+// CHALLENGE
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string)
+
+// METHOD 1
+const solution = (str, ending) => str.endsWith(ending)
