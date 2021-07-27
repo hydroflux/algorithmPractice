f7d501a3cb2b37637bbebfab7bb2936f47a22180
Jack Hubert <jphubert93@gmail.com>
2021-07-27 15:31:05 -0600

    completed method #1

--- challenges/twoSum.js
+++ challenges/twoSum.js
@@ -6,3 +6,15 @@
 // RESTRICTIONS
 // The input will always be valid (numbers will be an array of length 2 or greater, and all of the
 // items will be numbers; target will always be the sum of two different items from that array)
+
+// METHOD 1
+const twoSum(array, target){
+    for (let i = 0; i < numbers.length; i++){
+        let element = numbers[i]
+        let match = numbers.filter( value => numbers.indexOf(value) !== i )
+                           .find( value => value + element === target )
+        if (match){
+          return [i, numbers.indexOf(match)]
+        }
+      }
+}
