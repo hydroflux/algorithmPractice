18864a52a5095cc026d3751aba3faa0f9fbad82c
Jack Hubert <jphubert93@gmail.com>
2021-07-29 09:04:51 -0600

    forgot to save

--- challenges/playingWithDigits.js
+++ challenges/playingWithDigits.js
@@ -16,3 +16,4 @@ Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...)
 If it is the case we will return k, if not return -1.
 
 Note: n and p will always be given as strictly positive integers. */
+
