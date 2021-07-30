b99b5652aeddbb8de58e49d5b6104920c1eb507c
Jack Hubert <jphubert93@gmail.com>
2021-07-30 14:15:44 -0600

    added tests for writeNumberInExpandedForm

--- challenges/writeNumberInExpandedForm.js
+++ challenges/writeNumberInExpandedForm.js
@@ -21,3 +21,7 @@ function expandedForm(num) {
     }).join('')
 }
 
+// TESTING
+console.log( expandedForm(12) === '10 + 2' )
+console.log( expandedForm(42) === '40 + 2' )
+console.log( expandedForm(70304) === '70000 + 300 + 4' )
