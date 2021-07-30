df248af29dd23bcc81ccbcadb0ba18fdb95f2238
Jack Hubert <jphubert93@gmail.com>
2021-07-30 14:14:23 -0600

    Created first method of solving writeNumberInExpandedForm

--- challenges/writeNumberInExpandedForm.js
+++ challenges/writeNumberInExpandedForm.js
@@ -5,5 +5,19 @@ expandedForm(12); // Should return '10 + 2'
 expandedForm(42); // Should return '40 + 2'
 expandedForm(70304); // Should return '70000 + 300 + 4'
 
-NOTE: All numbers will be whole numbers greater than 0
+NOTE: All numbers will be whole numbers greater than 0 */
+
+// METHOD 1
+function expandedForm(num) {  
+    return String(num).split('').map( ( digit, index, array ) => {
+      if ( parseInt(digit) !== 0 ){
+        let conversion = digit
+        for ( i = 0; i < array.slice(index).length - 1; i ++ ){
+          conversion += "0"
+        }
+        
+        return index === 0 ? conversion : " + " + conversion
+      }
+    }).join('')
+}
 
