903e1be50590e471fb722103dc041fce05942f1f
Jack Hubert <jphubert93@gmail.com>
2021-08-02 16:19:59 -0600

    created a cleaner solution matching method1 but changing the syntax

--- challenges/whoLikesIt.js
+++ challenges/whoLikesIt.js
@@ -26,9 +26,25 @@ function likes(names) {
     }
 }
 
+// METHOD 1.5
+const likes2 = names => {
+    switch ( names.length ){
+        case 0: return `no one likes this`
+        case 1: return `${names[0]} likes this`
+        case 2: return `${names[0]} and ${names[1]} like this`
+        case 3: return `${names[0]}, ${names[1]} and ${names[2]} like this`
+        default: return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+    }
+}
+
 // TEST CASES
 console.log(likes([]) === 'no one likes this')
 console.log(likes(['Peter']) === 'Peter likes this');
 console.log(likes(['Jacob', 'Alex']) === 'Jacob and Alex like this');
 console.log(likes(['Max', 'John', 'Mark']) === 'Max, John and Mark like this');
 console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']) === 'Alex, Jacob and 2 others like this');
+console.log(likes2([]) === 'no one likes this')
+console.log(likes2(['Peter']) === 'Peter likes this');
+console.log(likes2(['Jacob', 'Alex']) === 'Jacob and Alex like this');
+console.log(likes2(['Max', 'John', 'Mark']) === 'Max, John and Mark like this');
+console.log(likes2(['Alex', 'Jacob', 'Mark', 'Max']) === 'Alex, Jacob and 2 others like this');
