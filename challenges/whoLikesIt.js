5d5ea841aa1d6d8ea2f9c15484d30892c584dd53
Jack Hubert <jphubert93@gmail.com>
2021-08-02 16:17:10 -0600

    added test cases for method 1 of 'whoLikesIt'

--- challenges/whoLikesIt.js
+++ challenges/whoLikesIt.js
@@ -25,3 +25,10 @@ function likes(names) {
           return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
     }
 }
+
+// TEST CASES
+console.log(likes([]) === 'no one likes this')
+console.log(likes(['Peter']) === 'Peter likes this');
+console.log(likes(['Jacob', 'Alex']) === 'Jacob and Alex like this');
+console.log(likes(['Max', 'John', 'Mark']) === 'Max, John and Mark like this');
+console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']) === 'Alex, Jacob and 2 others like this');
