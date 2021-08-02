7d8546ee07a58418dd72ce13faad6ac1ebb0bfb2
Jack Hubert <jphubert93@gmail.com>
2021-08-02 17:41:44 -0600

    set up tests for Method 1 encryption

--- challenges/encryption/alternatingSplit.js
+++ challenges/encryption/alternatingSplit.js
@@ -16,4 +16,25 @@ If the input-string is null or empty return exactly this value!
 If n is <= 0 then return the input text. */
 
 
-// METHOD 1
+// METHOD 1 - Encryption
+function encrypt(text, n) {
+    const alternatingSplit = (text, arg = 'even') => text.split('').filter( (char, index) => {
+      return arg == 'even' ? index % 2 === 0
+      : index % 2 !== 0
+    }).join('')
+    
+    for ( i = 0; i < n ; i ++ ){
+      text = `${alternatingSplit(text, 'odd')}${alternatingSplit(text,'even')}`
+    }
+  
+    return text
+}
+
+// TEST CASES
+console.log(encrypt("This is a test!", 0) === "This is a test!");
+console.log(encrypt("This is a test!", 1) === "hsi  etTi sats!");
+console.log(encrypt("This is a test!", 2) === "s eT ashi tist!");
+console.log(encrypt("This is a test!", 3) === " Tah itse sits!");
+console.log(encrypt("This is a test!", 4) === "This is a test!");
+console.log(encrypt("This is a test!", -1) === "This is a test!");
+console.log(encrypt("This kata is very interesting!", 1) === "hskt svr neetn!Ti aai eyitrsig");
