13ae737f5108bef59d60aa8a69e13dbe8f2610f9
Jack Hubert <jphubert93@gmail.com>
2021-08-02 16:15:19 -0600

    created a method of solving the whoLikesIt challenge

--- challenges/whoLikesIt.js
+++ challenges/whoLikesIt.js
@@ -10,3 +10,18 @@ likes ["Max", "John", "Mark"] -- must be "Max, John and Mark like this"
 likes ["Alex", "Jacob", "Mark", "Max"] -- must be "Alex, Jacob and 2 others like this"
 For 4 or more names, the number in and 2 others simply increases. */
 
+// METHOD 1
+function likes(names) {
+    switch ( names.length ){
+        case 0:
+          return `no one likes this`
+        case 1:
+          return `${names[0]} likes this`
+        case 2:
+          return `${names[0]} and ${names[1]} like this`
+        case 3:
+          return `${names[0]}, ${names[1]} and ${names[2]} like this`
+        default:
+          return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+    }
+}
