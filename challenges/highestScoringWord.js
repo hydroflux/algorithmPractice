7fe7be938a1120c206a7aed1a911eec283977f7f
Jack Hubert <jphubert93@gmail.com>
2021-08-03 08:29:41 -0600

    set up first method of solving the 'highScoringWord' challenge

--- challenges/highestScoringWord.js
+++ challenges/highestScoringWord.js
@@ -8,3 +8,12 @@ You need to return the highest scoring word as a string.
 If two words score the same, return the word that appears earliest in the original string.
 
 All letters will be lowercase and all inputs will be valid. */
+
+// METHOD 1
+function high(x){
+    const wordArray = x.split(' ')
+    const valueArray = wordArray.map( word => word.split('') )
+    const valueReducer = (acc, letter) => letter.charCodeAt(0) - 96 + acc
+    const reduced = valueArray.map( array => array.reduce(valueReducer, 0))
+    return wordArray[reduced.indexOf(Math.max(...reduced))]
+}
