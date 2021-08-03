ee687c79b4f594035aaed5b9bdaef7bbb4c1e401
Jack Hubert <jphubert93@gmail.com>
2021-08-03 08:30:22 -0600

    added test cases for Method 1

--- challenges/highestScoringWord.js
+++ challenges/highestScoringWord.js
@@ -17,3 +17,13 @@ function high(x){
     const reduced = valueArray.map( array => array.reduce(valueReducer, 0))
     return wordArray[reduced.indexOf(Math.max(...reduced))]
 }
+
+// TEST CASES
+console.log(high('man i need a taxi up to ubud') === 'taxi');
+console.log(high('what time are we climbing up the volcano') === 'volcano'); 
+console.log(high('take me to semynak') === 'semynak'); 
+console.log(high('aa b') === 'aa');
+console.log(high('b aa') === 'b');
+console.log(high('bb d') === 'bb');
+console.log(high('d bb') === 'd');
+console.log(high('aaa b') === 'aaa');
