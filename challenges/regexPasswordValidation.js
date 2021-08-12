050c8a124d0fcd5a5f30676c7987e84b30be6e13
Jack Hubert <jphubert93@gmail.com>
2021-08-11 19:14:27 -0600

    created method 1

--- challenges/regexPasswordValidation.js
+++ challenges/regexPasswordValidation.js
@@ -7,3 +7,17 @@ contains an uppercase letter
 contains a number
 Valid passwords will only be alphanumeric characters. */
 
+// METHOD 1
+function validate(password) {
+    //   AT LEAST 6 CHARACTERS LONG
+    //   ?!.*[&%$]).{6,}$
+    //   CONTAINS A LOWERCASE LETTER
+    //   ?=.*[A-Z]
+    //   CONTAINS AN UPPERCASE LETTER
+    //   ?=.*[a-z]
+    //   CONTAINS A NUMBER
+    //   ?=.*\d
+    //   NO CHARACTER MATCHES A SYMBOL
+    //   ?!.*[-!$%^&*()_+|~=`{}\[\]:";'<>?, .\/]
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?!.*[&%$])(?!.*[-!$%^&*()_+|~=`{}\[\]:";'<>?, .\/]).{6,}$/.test(password);
+}
