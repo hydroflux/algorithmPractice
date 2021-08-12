6da0bc1ee0e0559610fa01b2fa170edc28dfb541
Jack Hubert <jphubert93@gmail.com>
2021-08-11 19:17:58 -0600

    created tests for Method 1

--- challenges/regexPasswordValidation.js
+++ challenges/regexPasswordValidation.js
@@ -21,3 +21,13 @@ function validate(password) {
     //   ?!.*[-!$%^&*()_+|~=`{}\[\]:";'<>?, .\/]
     return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?!.*[&%$])(?!.*[-!$%^&*()_+|~=`{}\[\]:";'<>?, .\/]).{6,}$/.test(password);
 }
+
+// TESTS
+console.log(validate('djI38D55') === true)
+console.log(validate('a2.d412') === false)
+console.log(validate('JHD5FJ53') === false)
+Test.expect(validate('!fdjn345') === false)
+Test.expect(validate('jfkdfj3j') === false)
+Test.expect(validate('123') === false)
+Test.expect(validate('abc') === false)
+Test.expect(validate('Password123') === true)
