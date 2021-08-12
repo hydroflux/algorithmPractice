86418aa95af37b9629fbc0b006b3e79e8dd59c24
Jack Hubert <jphubert93@gmail.com>
2021-08-11 19:19:55 -0600

    created method 2 for regexPasswordValidation

--- challenges/regexPasswordValidation.js
+++ challenges/regexPasswordValidation.js
@@ -22,6 +22,16 @@ function validate(password) {
     return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?!.*[&%$])(?!.*[-!$%^&*()_+|~=`{}\[\]:";'<>?, .\/]).{6,}$/.test(password);
 }
 
+
+// METHOD 2
+const validate2 = password => {
+    return  /^[A-Za-z0-9]{6,}$/.test(password) &&
+            /[A-Z]+/           .test(password) &&
+            /[a-z]+/           .test(password) &&
+            /[0-9]+/           .test(password)
+  }
+
+
 // TESTS
 console.log(validate('djI38D55') === true)
 console.log(validate('a2.d412') === false)
@@ -31,3 +41,11 @@ console.log(validate('jfkdfj3j') === false)
 console.log(validate('123') === false)
 console.log(validate('abc') === false)
 console.log(validate('Password123') === true)
+console.log(validate2('djI38D55') === true)
+console.log(validate2('a2.d412') === false)
+console.log(validate2('JHD5FJ53') === false)
+console.log(validate2('!fdjn345') === false)
+console.log(validate2('jfkdfj3j') === false)
+console.log(validate2('123') === false)
+console.log(validate2('abc') === false)
+console.log(validate2('Password123') === true)
