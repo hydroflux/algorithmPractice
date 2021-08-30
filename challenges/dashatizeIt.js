1466ab89a9c551643c4a152bf882b1666d544650
Jack Hubert <jphubert93@gmail.com>
2021-08-30 17:19:48 -0600

    created first solution for the 'dashatizeIt' challenge

--- challenges/dashatizeIt.js
+++ challenges/dashatizeIt.js
@@ -10,3 +10,17 @@ Ex:
 dashatize(274) -> '2-7-4'
 dashatize(6815) -> '68-1-5' */
 
+// METHOD 1 => Status Quo
+const dashatize = number => {
+    if ( isNaN(number) ) return String(number)
+  
+    let dashedString = String(number)
+        .split('')
+        .map( int => int % 2 !== 0 ? `-${int}-` : int )
+        .join('')
+        .replace(/-{2,}/g, "-")
+
+    if ( dashedString[dashedString.length - 1] === '-' ) dashedString = dashedString.substring( 0, dashedString.length - 1 )
+    if ( dashedString[0] === '-' ) dashedString = dashedString.substring( 1, dashedString.length )
+    return dashedString
+}
