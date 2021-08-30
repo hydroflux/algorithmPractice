b5f8e5945a3b91944158be8ea29040b76ec3e2b9
Jack Hubert <jphubert93@gmail.com>
2021-08-30 13:09:43 -0600

    built out method1 solution to 'domainNameExtraction' using the first methods that come to mind

--- challenges/domainNameExtraction.js
+++ challenges/domainNameExtraction.js
@@ -6,3 +6,17 @@ domainName("http://www.zombie-bites.com") == "zombie-bites"
 domainName("https://www.cnet.com") == "cnet"
 */
 
+// METHOD 1 => Status Quo
+const domainName = url => {
+    let protocolIndex = url.indexOf('//') + 2
+    if ( url.slice( protocolIndex ).startsWith('www.') ) protocolIndex += 4
+    if ( protocolIndex === 1 ) protocolIndex = url.indexOf('www.') + 4
+    if ( protocolIndex === 3 ) protocolIndex = 0
+
+    let host = url.slice( protocolIndex )
+
+    return host
+      .slice( 0, host.indexOf('.') )
+}
+
+console.log(domainName('http://www.zombie-bites.com'))
