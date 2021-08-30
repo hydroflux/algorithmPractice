056615f77f40336e5b3f1d5681f7a8d8a9eec70b
Jack Hubert <jphubert93@gmail.com>
2021-08-30 15:43:56 -0600

    set up Method 2 to solve the 'findTheOddInt' challenge after reviewing additional code war solutions

--- challenges/findTheOddInt.js
+++ challenges/findTheOddInt.js
@@ -10,7 +10,7 @@ Examples
 [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd). */
 
-// METHOD 1
+// METHOD 1 => Status Quo
 const findOdd = array => {
     let counter = {}
     array.forEach( number => {
@@ -25,6 +25,9 @@ const findOdd = array => {
 
     return 0
 }
+
+// METHOD 2
+const findOdd2 = array => array.reduce( ( c , v ) => c^v , 0 )
 
 // TESTING
 const array1 = [1,2,2,3,3,3,4,3,3,3,2,2,1]
