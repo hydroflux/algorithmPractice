8d341c76092696f3976c72a03da189dff6fa8070
Jack Hubert <jphubert93@gmail.com>
2021-08-30 15:37:29 -0600

    created Method 1 to solve the 'findTheOddInt' challenge

--- challenges/findTheOddInt.js
+++ challenges/findTheOddInt.js
@@ -8,4 +8,20 @@ Examples
 [0] should return 0, because it occurs 1 time (which is odd).
 [1,1,2] should return 2, because it occurs 1 time (which is odd).
 [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
-[1,2,2,3,3,3,4,3,3,3,2,2,1] shold return 4, because it appears 1 time (which is odd). */
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd). */
+
+// METHOD 1
+const findOdd = array => {
+    let counter = {}
+    array.forEach( number => {
+        counter[ number ]
+        ? counter[ number ] += 1
+        : counter[ number ] = 1
+    })
+
+    for ( let [ key, value ] of Object.entries( counter )){
+        if ( value % 2 !== 0 ) return parseInt( key )
+    }
+
+    return 0
+}
