db3b6ca6a23b13df3f7231277888524f904ad675
Jack Hubert <jphubert93@gmail.com>
2021-08-30 13:13:04 -0600

    added tests for 'domainNameExtraction' Method 1

--- challenges/domainNameExtraction.js
+++ challenges/domainNameExtraction.js
@@ -19,4 +19,17 @@ const domainName = url => {
       .slice( 0, host.indexOf('.') )
 }
 
-console.log(domainName('http://www.zombie-bites.com'))
+// TESTING
+const url1 = 'http://github.com/carbonfive/raygun'
+const url2 = 'http://google.co.jp'
+const url3 = 'http://pvio-br2x0w80tpx.edu/index.php'
+const url4 = 'https://www.9am-2hespjyl-f0m2mkpi39ns5y.co.uk/img/'
+const url5 = '0syl3aw0a2.de'
+const url6 = 'http://www.loxcdy1oano6t13uk0-8qjs-vf.co/archive/'
+
+console.log( domainName( url1 ) === 'github' )
+console.log( domainName( url2 ) === 'google' )
+console.log( domainName( url3 ) === 'pvio-br2x0w80tpx' )
+console.log( domainName( url4 ) === '9am-2hespjyl-f0m2mkpi39ns5y' )
+console.log( domainName( url5 ) === '0syl3aw0a2' )
+console.log( domainName( url6 ) === 'loxcdy1oano6t13uk0-8qjs-vf' )
