46ed5cc98c2475a8782d7008e73b21e920c5a260
Jack Hubert <jphubert93@gmail.com>
2021-08-31 16:24:00 -0600

    unable to solve 'removeZeros' without using temporary objects , arrays, or prototype functions / methods; incredibly frustrating to see temporary objects in the solution---consider stop using code wars for challenges if the number of bullshit challenge / solution combinations continue to appear

--- challenges/removeZeros.js
+++ challenges/removeZeros.js
@@ -22,6 +22,40 @@ const removeZeros = array => {
                     .map( value => parseInt( value )))
 }
 
+// METHOD 2 => .concat() , .filter(), & .map() are prohibited
+const removeZeros2 = array => {
+      // Sort "array" so that all elements with the value of zero are moved to the
+  // end of the array, while the other elements maintain order.
+  // [0, 1, 2, 0, 3] --> [1, 2, 3, 0, 0]
+  // Zero elements also maintain order in which they occurred.
+  // [0, "0", 1, 2, 3] --> [1, 2, 3, 0, "0"]
+  
+  // Do not use any temporary arrays or objects. Additionally, you're not able
+  // to use any Array or Object prototype methods such as .shift(), .push(), etc
+  
+  // the correctly sorted array should be returned.
+
+  if (array.length === 0) return array;
+  let zeroCount = 0; //keeps track of zeros so we know when to exit for loop
+  for (let i = 0, len = array.length; i < len; i++) {
+      if (array[i] === 0 || array[i] === '0') { //shuffle numbers back one space and put zero at end
+          zeroCount += 1;
+
+          for (let j = i + 1; j < len; j++) {//this looks for non-zero after i of array
+              let itemToMoveUp = array[j]; //non-zero number at end to swap places with zero num
+              array[j] = array[j-1]; //set index of num we will move up to zero
+              array[j-1] = itemToMoveUp; //now the zero has been swapped for a non-zero
+          }
+
+          i -= 1; //move index back one because next item is now one index lower
+      }
+      if (i + 1 + zeroCount >= len){ //if only zeros that were moved are left -> end
+          return array;
+      }
+  }
+}
+
+
 // TESTING
 const arraysEqual = ( a , b ) => {
     if ( a === b ) return true
@@ -38,3 +72,4 @@ const arraysEqual = ( a , b ) => {
 const input = [7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14]
 const solution = [7, 2, 3, 4, 6, 13, 78, 19, 14, 0, 0, 0, 0, 0, 0]
 console.log( arraysEqual( removeZeros( input ) , solution ) )
+console.log( arraysEqual( removeZeros2( input ) , solution ) )
