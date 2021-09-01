e190d61e1bbe7cbae3153e5bf3588e9f88e3a8a6
Jack Hubert <jphubert93@gmail.com>
2021-09-01 10:09:14 -0600

    created Method 1 solution for 'permutations' challenge---using recursion to solve the permutations problem is a bit challenging to wrap my head around, need to learn more

--- challenges/permutations.js
+++ challenges/permutations.js
@@ -7,3 +7,30 @@ permutations('a'); // ['a']
 permutations('ab'); // ['ab', 'ba']
 permutations('aabb'); // ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
 The order of the permutations doesn't matter. */
+
+import { equivalentArrays } from "../usefulFunctions/equivalentArrays"
+
+// METHOD 1 => Status Quo
+const permutations = string => {
+    let characterArray = string.split('')
+    let permutationsHash = {}
+    let permutationsArray = []
+
+    const permute = ( array, m = [] ) => {
+        if ( array.length === 0 ){
+            if ( !permutationsHash[ m ] ){
+                permutationsHash[ m ] = 1 &&
+                permutationsArray.push( m.join('') )
+            }
+        } else {
+            for ( let i = 0 ; i < array.length ; i ++ ){
+                let current = array.slice()
+                let next = current.splice( i, 1 )
+                permute( current.slice() , m.concat(next) )
+            }
+        }
+    }
+    
+    permute( characterArray )
+    return permutationsArray
+}
