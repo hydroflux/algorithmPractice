da4072e4a6eeaf53ac9e7b91cfc6f806a65bc503
Jack Hubert <jphubert93@gmail.com>
2021-09-02 08:46:08 -0600

    created Method 1 solution for 'convertBooleanValues'

--- ruby/challenges/convertBooleanValues.rb
+++ ruby/challenges/convertBooleanValues.rb
@@ -2,3 +2,8 @@
 =begin
     Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 =end
+
+# METHOD 1 => Status Quo
+def bool_to_word boolean
+    boolean == true ? "Yes" : "No"
+end
