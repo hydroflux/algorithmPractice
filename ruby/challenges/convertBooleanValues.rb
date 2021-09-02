708f04533e0c8fc595d103c27a1b108a81cb3d2f
Jack Hubert <jphubert93@gmail.com>
2021-09-02 08:45:37 -0600

    created a 'ruby' folder & a 'challenges' folder for ruby algorithm practice; set up 'convertBooleanValues' challenge from code wars

---
+++ ruby/challenges/convertBooleanValues.rb
@@ -0,0 +1,4 @@
+# CHALLENGE => Convert Boolean Values to Strings 'Yes' or 'No'
+=begin
+    Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+=end
