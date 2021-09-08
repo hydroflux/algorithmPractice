5af046ab6021175d0878d8d9774901ee3ad13a4d
Jack Hubert <jphubert93@gmail.com>
2021-09-08 08:50:18 -0600

    added test cases for Method 1 solution of 'countingSheep' kata

--- ruby/challenges/countingSheep.rb
+++ ruby/challenges/countingSheep.rb
@@ -17,3 +17,14 @@ def countSheeps array
     array.select { | value | value == true }
          .reduce(0) { | acc, value | acc + 1 }
 end
+
+# Testing
+array_1 = [ true,  true,  true,  false, true,  true,  true,  true, true,  false, true,  false,
+            true,  false, false, true, true,  true,  true,  true, false, false, true,  true ]
+array_2 = [ true, true, true, true, true, true, true, true, true ]
+array_3 = [ false, false, false, false, false, false, false, false, false ]
+
+# Method 1
+p countSheeps( array_1 ) == 17
+p countSheeps( array_2 ) == 9
+p countSheeps( array_3 ) == 0
