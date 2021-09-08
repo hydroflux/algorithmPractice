931d732100c8ea88722f56be20cbc251ae5b4a0b
Jack Hubert <jphubert93@gmail.com>
2021-09-08 08:32:49 -0600

    added tests for 'disemvowelTrolls' Method 1

--- ruby/challenges/disemvowelTrolls.rb
+++ ruby/challenges/disemvowelTrolls.rb
@@ -13,3 +13,12 @@
 def disemvowel string
     string.split('').filter { | char | !['a', 'A', 'e', 'E', 'i', 'I', 'o',  'O', 'u', 'U'].include? char }.join
 end
+
+# TESTING
+string_1 = 'The quick brown fox jumped over the lazy dog'
+string_2 = 'Hello operator, I would like to place a call'
+string_3 = 'Bananaphone'
+
+p disemvowel(string_1) == 'Th qck brwn fx jmpd vr th lzy dg'
+p disemvowel(string_2) == 'Hll prtr,  wld lk t plc  cll'
+p disemvowel(string_3) == 'Bnnphn'
