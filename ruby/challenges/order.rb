b1bf1ec9832df2ac689c362cd34166b5cbbff34a
Jack Hubert <jphubert93@gmail.com>
2021-09-09 10:13:28 -0600

    added tests for Method 1 solution for 'order' challenge

--- ruby/challenges/order.rb
+++ ruby/challenges/order.rb
@@ -23,3 +23,22 @@ def order words
        .map { | value | value[1] }
        .join(' ')
 end
+
+# Testing
+string_1 = 'is2 Thi1s T4est 3a'
+string_2 = '4of Fo1r pe6ople g3ood th5e the2'
+string_3 = ''
+string_4 = 'y3oung abo6ut ba8d their5 old1 p2erson lo4ng she7 9be'
+string_5 = 'o4ld woul2d smal5l important1 p8art g3overnment r6ight 7about'
+
+solution_1 = 'Thi1s is2 3a T4est'
+solution_2 = 'Fo1r the2 g3ood 4of th5e pe6ople'
+solution_3 = ''
+solution_4 = 'old1 p2erson y3oung lo4ng their5 abo6ut she7 ba8d 9be'
+solution_5 = 'important1 woul2d g3overnment o4ld smal5l r6ight 7about p8art'
+
+p order(string_1) == solution_1
+p order(string_2) == solution_2
+p order(string_3) == solution_3
+p order(string_4) == solution_4
+p order(string_5) == solution_5
