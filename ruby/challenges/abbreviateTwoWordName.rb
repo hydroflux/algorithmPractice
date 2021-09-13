34afab94f0667db7d8c09db603d2dd3bf25979d1
Jack Hubert <jphubert93@gmail.com>
2021-09-13 10:16:16 -0600

    created Method 1 solution for 'abbreviateTwoWordName' challenge from code wars

--- ruby/challenges/abbreviateTwoWordName.rb
+++ ruby/challenges/abbreviateTwoWordName.rb
@@ -11,3 +11,26 @@
 
   patrick feeney => P.F
 =end
+
+# Method 1 => Status Quo
+def abbrev_name name
+  name.split()
+      .map { | word | word[0].upcase }
+      .join('.')
+end
+
+name_1 = 'Sam Harris'
+abbrev_1 = "S.H"
+
+describe "Fixed tests" do
+  it "should pass fixed tests" do
+    Test.assert_equals(abbrev_name("Sam Harris"), "S.H")
+    Test.assert_equals(abbrev_name("Patrick Feenan"), "P.F")
+    Test.assert_equals(abbrev_name("Evan Cole"), "E.C")
+    Test.assert_equals(abbrev_name("P Favuzzi"), "P.F")
+    Test.assert_equals(abbrev_name("David Mendieta"), "D.M")
+  end
+
+
+
+p abbrev_name(name_1)
