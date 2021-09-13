8043f08a583c2856ba629a023b7d5ed454f415d6
Jack Hubert <jphubert93@gmail.com>
2021-09-13 10:21:35 -0600

    added a second method solution to the 'abbreviateTwoNames' challenge after reviewing other solutions and learning about the 'chr' string method in Ruby

--- ruby/challenges/abbreviateTwoWordName.rb
+++ ruby/challenges/abbreviateTwoWordName.rb
@@ -19,6 +19,16 @@ def abbrev_name name
       .join('.')
 end
 
+# Method 2 => Using 'map' & 'chr'
+def abbrev_name_2 name
+  name.upcase
+      .split
+      .map(&:chr)
+      .join('.')
+end
+
+# chr is a String class method in Ruby which is used to return a one-character string at the beginning of the string.
+
 # Testing
 name_1 = 'Sam Harris'
 name_2 = 'Patrick Freeman'
@@ -31,8 +41,16 @@ abbrev_3 = 'E.C'
 abbrev_4 = 'P.F'
 abbrev_5 = 'D.M'
 
+# Method 1 Testing
 p abbrev_name(name_1) == abbrev_1
 p abbrev_name(name_2) == abbrev_2
 p abbrev_name(name_3) == abbrev_3
 p abbrev_name(name_4) == abbrev_4
 p abbrev_name(name_5) == abbrev_5
+
+# Method 2 Testing
+p abbrev_name_2(name_1) == abbrev_1
+p abbrev_name_2(name_2) == abbrev_2
+p abbrev_name_2(name_3) == abbrev_3
+p abbrev_name_2(name_4) == abbrev_4
+p abbrev_name_2(name_5) == abbrev_5
