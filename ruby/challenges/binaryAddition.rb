396fcb4d6c5582cde0834b2d92c5d11169e8367d
Jack Hubert <jphubert93@gmail.com>
2021-09-15 10:33:06 -0600

    created Method 1 solution to 'binaryAddition' ruby challenge

--- ruby/challenges/binaryAddition.rb
+++ ruby/challenges/binaryAddition.rb
@@ -10,3 +10,8 @@
   add_binary(1, 1) == "10" (1 + 1 = 2 in decimal or 10 in binary)
   add_binary(5, 9) == "1110" (5 + 9 = 14 in decimal or 1110 in binary)
 =end
+
+# METHOD 1 => Status Quo
+def add_binary a, b
+  (a + b).to_s(2)
+end
