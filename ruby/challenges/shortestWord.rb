fca7a32a3c6de646e2046a20e50de7746ca96984
Jack Hubert <jphubert93@gmail.com>
2021-09-15 10:44:51 -0600

    created method 1 solution for 'shortestWord' challenge

--- ruby/challenges/shortestWord.rb
+++ ruby/challenges/shortestWord.rb
@@ -5,3 +5,8 @@
   String will never be empty and you do not need to account for different data types.
 =end
 
+def find_short string
+  string.split
+        .reduce { | shortest, word | word.length < shortest.length ? word : shortest }
+        .length
+end
