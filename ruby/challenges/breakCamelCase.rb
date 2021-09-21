538f32c814765cdefca43a6d5b59748437cf81f1
Jack Hubert <jphubert93@gmail.com>
2021-09-21 11:29:33 -0600

    created a Method 1 solution for 'breakCamelCase' challenge; added tests for Method 1

--- ruby/challenges/breakCamelCase.rb
+++ ruby/challenges/breakCamelCase.rb
@@ -7,3 +7,27 @@
   "identifier"   =>  "identifier"
   ""             =>  ""
 =end
+
+# Method 1 => Status Quo
+def breakCamelCase string
+  string.split('')
+        .map { | char | char == char.capitalize ? " #{char}" : char }
+        .join()
+end
+
+# Test Cases
+string_1 = "camelCasing"
+string_2 = "identifier"
+string_3 = "yearCompanyLeaveFind"
+string_4 = "factNounsDifferentWay"
+
+solution_1 = "camel Casing"
+solution_2 = "identifier"
+solution_3 = "year Company Leave Find"
+solution_4 = "fact Nouns Different Way"
+
+# Method 1 Testing
+p breakCamelCase(string_1) == solution_1
+p breakCamelCase(string_2) == solution_2
+p breakCamelCase(string_3) == solution_3
+p breakCamelCase(string_4) == solution_4
