e9527c29e6fe7d68482a0c0ee2c18b34d8c1d6cc
Jack Hubert <jphubert93@gmail.com>
2021-09-22 10:02:24 -0600

    added additional test cases for 'sumDigPow' code challenge

--- ruby/challenges/sumDigPow.rb
+++ ruby/challenges/sumDigPow.rb
@@ -40,9 +40,27 @@ end
 
 # Test Cases
 a_1 = 1
+a_2 = 10
+a_3 = 90
+a_4 = 50
+
 b_1 = 10
+b_2 = 100
+b_3 = 150
 
 solution_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+solution_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+solution_3 = [89]
+solution_4 = []
+solution_5 = [135]
+solution_6 = [89, 135]
+solution_7 = [89, 135]
 
 # Method 1 Testing
 p sum_dig_pow(a_1, b_1) == solution_1
+p sum_dig_pow(a_1, b_2) == solution_2
+p sum_dig_pow(a_2, b_2) == solution_3
+p sum_dig_pow(a_3, b_2) == solution_4
+p sum_dig_pow(a_3, b_3) == solution_5
+p sum_dig_pow(a_4, b_3) == solution_6
+p sum_dig_pow(a_2, b_3) == solution_7
