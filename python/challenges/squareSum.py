e977409718423bf7917a9407da2a52809c6b91c7
Jack Hubert <jphubert93@gmail.com>
2021-09-27 15:04:12 -0600

    created a Method 1 solution for the 'squareSum' challenge

--- python/challenges/squareSum.py
+++ python/challenges/squareSum.py
@@ -6,3 +6,16 @@
     For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 '''
 
+# Method 1 => Status Quo
+def square_sum(numbers):
+    return sum([char * char for char in numbers])
+
+
+# Test Cases
+array_1 = [1, 2, 2]
+
+solution_1 = 9
+
+
+# Method 1 Testing
+print(square_sum(array_1) == solution_1)
