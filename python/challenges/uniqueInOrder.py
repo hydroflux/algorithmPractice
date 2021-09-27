6e6d82a2a8995b7c99a09ad7e6a4541514465c9f
Jack Hubert <jphubert93@gmail.com>
2021-09-27 13:13:50 -0600

    added additional tests for 'uniqueInOrder' python challenge

--- python/challenges/uniqueInOrder.py
+++ python/challenges/uniqueInOrder.py
@@ -21,9 +21,18 @@ def unique_in_order(iterable):
 
 
 # Test Cases
-string_1 = 'AAAABBBCCDAABBB'
+iterable_1 = 'AAAABBBCCDAABBB'
+iterable_2 = 'ABBCcAD'
+iterable_3 = [1, 2, 3, 4]
+iterable_4 = [1, 2, 2, 3, 3]
 
 solution_1 = ['A', 'B', 'C', 'D', 'A', 'B']
+solution_2 = ['A', 'B', 'C', 'c', 'A', 'D']
+solution_3 = [1, 2, 3, 4]
+solution_4 = [1, 2, 3]
 
 # Method 1 Testing
-print(unique_in_order(string_1) == solution_1)
+print(unique_in_order(iterable_1) == solution_1)
+print(unique_in_order(iterable_2) == solution_2)
+print(unique_in_order(iterable_3) == solution_3)
+print(unique_in_order(iterable_4) == solution_4)
