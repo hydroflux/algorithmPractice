b5e5d8885cfc6152052813d76b5bf43cad8bd161
Jack Hubert <jphubert93@gmail.com>
2021-09-27 15:05:25 -0600

    added additional test cases for the Method 1 solution for 'squareSum'

--- python/challenges/squareSum.py
+++ python/challenges/squareSum.py
@@ -13,9 +13,18 @@ def square_sum(numbers):
 
 # Test Cases
 array_1 = [1, 2, 2]
+array_2 = [0, 3, 4, 5]
+array_3 = []
+array_4 = [-1, 0, 1]
 
 solution_1 = 9
+solution_2 = 50
+solution_3 = 0
+solution_4 = 2
 
 
 # Method 1 Testing
 print(square_sum(array_1) == solution_1)
+print(square_sum(array_2) == solution_2)
+print(square_sum(array_3) == solution_3)
+print(square_sum(array_4) == solution_4)
