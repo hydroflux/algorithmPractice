d532e3f2992efce3f96d482b10712748c34223ce
Jack Hubert <jphubert93@gmail.com>
2021-09-28 12:38:44 -0600

    created a Method 1 solution for 'beginnerSeries2Clock'; added test case

--- python/challenges/beginnerSeries2Clock.py
+++ python/challenges/beginnerSeries2Clock.py
@@ -18,3 +18,16 @@
     0 <= s <= 59
 '''
 
+# Method 1 => Status Quo
+def past(h, m, s):
+    return ((h*60 + m)*60 + s)*1000
+
+
+# Test Cases
+h1 = 0
+m1 = 1
+s1 = 1
+
+
+# Method 1 Testing
+print(past(h1, m1, s1) == 61000)
