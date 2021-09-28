ed338558c695dfd53a9173f73eedb92b7e83f0f8
Jack Hubert <jphubert93@gmail.com>
2021-09-28 12:21:29 -0600

    added additional test cases for 'sumTwoLowestIntegers' challenge

--- python/challenges/sumTwoLowestIntegers.py
+++ python/challenges/sumTwoLowestIntegers.py
@@ -15,7 +15,17 @@ def sum_two_smallest_numbers(numbers):
 
 # Test Cases
 array_1 = [10, 343445353, 3453445, 3453545353453]
+array_2 = [5, 8, 12, 18, 22]
+array_3 = [7, 15, 12, 18, 22]
+array_4 = [25, 42, 12, 18, 22]
+
 solution_1 = 3453455
+solution_2 = 13
+solution_3 = 19
+solution_4 = 30
 
 # Method 1 Testing
 print(sum_two_smallest_numbers(array_1) == solution_1)
+print(sum_two_smallest_numbers(array_2) == solution_2)
+print(sum_two_smallest_numbers(array_3) == solution_3)
+print(sum_two_smallest_numbers(array_4) == solution_4)
