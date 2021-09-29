42a95bed3c6dc93fc5889f18645917f92d3f0aaa
Jack Hubert <jphubert93@gmail.com>
2021-09-29 11:47:19 -0600

    created Method 2 solution with tests after reviewing code wars solutions

--- python/challenges/primeFactorDecomposition.py
+++ python/challenges/primeFactorDecomposition.py
@@ -26,12 +26,24 @@ def prime_factors(n, i=2):
             n //= int(prime_factor[0] ** prime_factor[1])
         i += 1
     
-    if n > 0:
+    if n > 1:
         prime_factors.append((n, f'({n})'))
 
     prime_factors.sort()
     return ('').join(factor[1] for factor in prime_factors)
 
+
+# Method 2 => Using Python 'String Formatting' & Using Colons For While Statements
+def primeFactors(n):
+    i, j, p = 2, 0, []
+    while n > 1:
+        while n % i == 0: n, j = n/i, j + 1
+        if j > 0: p.append([i, j])
+        i, j = i + 1, 0
+    return ''.join('(%d' %q[0] + ('**%d' %q[1]) * (q[1] > 1) + ')' for q in p)
+
+# The '* (q[1] > 1)' is acting as a equivalency statement to add the second factor by multiplying the second factor
+# 1 time if the statement is true
 
 # Test Cases
 n1 = 100
@@ -58,3 +70,12 @@ print(prime_factors(n4) == solution_4)
 print(prime_factors(n5) == solution_5)
 print(prime_factors(n6) == solution_6)
 print(prime_factors(n7) == solution_7)
+
+# Method 2 Testing
+print(primeFactors(n1) == solution_1)
+print(primeFactors(n2) == solution_2)
+print(primeFactors(n3) == solution_3)
+print(primeFactors(n4) == solution_4)
+print(primeFactors(n5) == solution_5)
+print(primeFactors(n6) == solution_6)
+print(primeFactors(n7) == solution_7)
