b1d2a7b865757653711c424124685e974fd4bcaf
Jack Hubert <jphubert93@gmail.com>
2021-09-30 10:54:42 -0600

    created Method 2 solution by simplifying Method 1 for 'roundUpMultipleOf5'; added test cases

--- python/challenges/roundUpMultipleOf5.py
+++ python/challenges/roundUpMultipleOf5.py
@@ -27,6 +27,12 @@ def round_to_next5(n):
     return n
 
 
+# Method 1 => Reduced Method 1
+def roundToNext5(n):
+    while n % 5 != 0: n += 1
+    return n
+
+
 # Test Cases
 n1 = 0
 n2 = 2
@@ -35,10 +41,25 @@ n4 = 14
 n5 = 96
 n6 = 129341
 
+solution_1 = 0
+solution_2 = 5
+solution_3 = 5
+solution_4 = 15
+solution_5 = 100
+solution_6 = 129345
+
 # Method 1 Testing
-print(round_to_next5(n1) == 0)
-print(round_to_next5(n2) == 5)
-print(round_to_next5(n3) == 5)
-print(round_to_next5(n4) == 15)
-print(round_to_next5(n5) == 100)
-print(round_to_next5(n6) == 129345)
+print(round_to_next5(n1) == solution_1)
+print(round_to_next5(n2) == solution_2)
+print(round_to_next5(n3) == solution_3)
+print(round_to_next5(n4) == solution_4)
+print(round_to_next5(n5) == solution_5)
+print(round_to_next5(n6) == solution_6)
+
+# Method 2 Testing
+print(roundToNext5(n1) == solution_1)
+print(roundToNext5(n2) == solution_2)
+print(roundToNext5(n3) == solution_3)
+print(roundToNext5(n4) == solution_4)
+print(roundToNext5(n5) == solution_5)
+print(roundToNext5(n6) == solution_6)
