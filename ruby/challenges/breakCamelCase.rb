76c538a2b12b5f9b9e51a08c5d2c5f1d0b1e000c
Jack Hubert <jphubert93@gmail.com>
2021-10-01 10:43:08 -0600

    created a .rubocop.yml file to configure rubocops functionality; updated breakCamelCase.rb with the new linter--need to update other ruby files; created an npm_modules packages for js

--- ruby/challenges/breakCamelCase.rb
+++ ruby/challenges/breakCamelCase.rb
@@ -3,53 +3,53 @@
   Complete the solution so that the function will break up camel casing, using a space between words.
 
   Example
-  "camelCasing"  =>  "camel Casing"
-  "identifier"   =>  "identifier"
-  ""             =>  ""
+  'camelCasing'  =>  'camel Casing'
+  'identifier'   =>  'identifier'
+  ''             =>  ''
 =end
 
 # Method 1 => Status Quo
 def breakCamelCase string
   string.split('')
         .map { | char | char == char.capitalize ? " #{char}" : char }
-        .join()
+        .join
 end
 
 # Method 2 => Using 'gsub' and Regex
-def breakCamelCase_2 string
-  string.gsub /([A-Z])/, ' \1'
+def breakCamelCase2 string
+  string.gsub(/([A-Z])/, ' \1')
 end
 
 # 'gsub' is using regex to check for a capital letter & adding a leading space where it is found
 
 # Method 3 => Method 2 (Single Line)
-def breakCamelCase_3(string) string.gsub /([A-Z])/, ' \1' end
+def breakCamelCase3(string) string.gsub /([A-Z])/, ' \1' end
 
 # Test Cases
-string_1 = "camelCasing"
-string_2 = "identifier"
-string_3 = "yearCompanyLeaveFind"
-string_4 = "factNounsDifferentWay"
+string1 = 'camelCasing'
+string2 = 'identifier'
+string3 = 'yearCompanyLeaveFind'
+string4 = 'factNounsDifferentWay'
 
-solution_1 = "camel Casing"
-solution_2 = "identifier"
-solution_3 = "year Company Leave Find"
-solution_4 = "fact Nouns Different Way"
+solution1 = 'camel Casing'
+solution2 = 'identifier'
+solution3 = 'year Company Leave Find'
+solution4 = 'fact Nouns Different Way'
 
 # Method 1 Testing
-p breakCamelCase(string_1) == solution_1
-p breakCamelCase(string_2) == solution_2
-p breakCamelCase(string_3) == solution_3
-p breakCamelCase(string_4) == solution_4
+p breakCamelCase(string1) == solution1
+p breakCamelCase(string2) == solution2
+p breakCamelCase(string3) == solution3
+p breakCamelCase(string4) == solution4
 
 # Method 2 Testing
-p breakCamelCase_2(string_1) == solution_1
-p breakCamelCase_2(string_2) == solution_2
-p breakCamelCase_2(string_3) == solution_3
-p breakCamelCase_2(string_4) == solution_4
+p breakCamelCase2(string1) == solution1
+p breakCamelCase2(string2) == solution2
+p breakCamelCase2(string3) == solution3
+p breakCamelCase2(string4) == solution4
 
 # Method 3 Testing
-p breakCamelCase_3(string_1) == solution_1
-p breakCamelCase_3(string_2) == solution_2
-p breakCamelCase_3(string_3) == solution_3
-p breakCamelCase_3(string_4) == solution_4
+p breakCamelCase3(string1) == solution1
+p breakCamelCase3(string2) == solution2
+p breakCamelCase3(string3) == solution3
+p breakCamelCase3(string4) == solution4
