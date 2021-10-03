2d653c48a5878bf935748c53bc12c118f9d8ca74
Jack Hubert <jphubert93@gmail.com>
2021-10-03 11:41:30 -0600

    updated Method 1 solution after added additional test cases to solve

--- python/challenges/sumOfPairs.py
+++ python/challenges/sumOfPairs.py
@@ -32,17 +32,53 @@
 
 # Method 1 => Status Quo
 def sum_pairs(array, total):
-    for index in range(len(array)):
-        if sum(array[index: index + 2]) == total:
-            return array[index: index+2]
+    pairs = []
+    distance = len(array)
+    for i in range(len(array)):
+        for j in range(len(array) - (i + 1)):
+            k = array[i + 1:][j]
+            j += (i + 1)
+            if array[i] + k == total:
+                if j - i < distance:
+                    pairs = [array[i], k]
+                    distance = j - i
+                    if distance == 1: return pairs
+    if pairs: return pairs
+                
 
 
 # Test Cases
 array_1 = [11, 3, 7, 5]
 sum_1 = 10
 
+array_2 = [1, 4, 8, 7, 3, 15]
+sum_2 = 8
+
+array_3 = [10, 5, 2, 3, 7, 5]
+sum_3 = 10
+
+array_4 = [20, -13, 40]
+sum_4 = -7
+
+array_5 = [10, 5, 2, 3, 7, 5]
+sum_5 = 10
+
+array_6 = [1, 2, 3, 4, 1, 0]
+sum_6 = 2
+
 solution_1 = [3, 7]
+solution_2 = [1, 7]
+solution_3 = [5, 5]
+solution_4 = None
+solution_5 = [3, 7]
+solution_6 = [1, 1]
 
 
 # Method 1 Testing
-print(sum_pairs(array_1, sum_1) == solution_1)
+# print(sum_pairs(array_1, sum_1) == solution_1)
+# print(sum_pairs(array_2, sum_2) == solution_2)
+# print(sum_pairs(array_3, sum_3) == solution_3)
+# print(sum_pairs(array_4, sum_4) == solution_4)
+# print(sum_pairs(array_5, sum_5) == solution_5)
+# print(sum_pairs(array_6, sum_6) == solution_6)
+print(sum_pairs(array_6, sum_6))
