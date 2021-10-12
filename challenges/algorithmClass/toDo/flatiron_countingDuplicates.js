dda4c72304edf6b5c183b54b416d8300bbfb1eb1
Jack Hubert <jphubert93@gmail.com>
2021-10-12 15:21:45 -0600

    completed class method solution 'flatirno_countingDuplicates' solution

--- challenges/algorithmClass/toDo/flatiron_countingDuplicates.js
+++ challenges/algorithmClass/toDo/flatiron_countingDuplicates.js
@@ -1,7 +1,7 @@
 // CHALLENGE => Count the Number of Duplicates
 /* Write a function that will:
-    1) Return the count of distinct case-insensitive alphabetic characters and
-    2) Numeric digits that occur more than once in the input string.
+    1) Return the count of distinct case-insensitive alphabetic characters that occur more than once in the input string
+    2) And numeric digits that occur more than once in the input string
     3) The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
 
 Example
@@ -12,3 +12,45 @@ Example
 "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
 "aA11" -> 2 # 'a' and '1'
 "ABBA" -> 2 # 'A' and 'B' each occur twice */
+
+// Method 1 => Status Quo
+const countNumberDuplicates = (string) => {
+    let counter = []
+    let firstCount = []
+    let duplicates = []
+    const splitArray = string.toLowerCase().split('')
+    splitArray.map(
+        character => {
+            if ( firstCount.includes(character) ){
+                duplicates.push(character)
+            } else {
+                firstCount.push(character)
+            }
+        }
+    )
+
+    duplicates.map( duplicate => {
+        if ( !counter.includes(duplicate)){
+            counter.push(duplicate)
+        }
+    })
+
+    return counter.length
+}
+
+
+// Test Cases
+let string_1 = "aBcde"
+let string_2 = "aabbcde"
+let string_3 = 'Indivisibilities'
+let string_4 = 'abcdefghABCDabcABA ?'
+let string_5 = 'abcdefghijklmnABCDEFGabcdefabcdeABCDabcABA'
+let string_6 = 'abcdefABCABa'
+
+// Method 1 Testing
+console.log(countNumberDuplicates(string_1) === 0)
+console.log(countNumberDuplicates(string_2) === 2)
+console.log(countNumberDuplicates(string_3) === 2)
+console.log(countNumberDuplicates(string_4) === 4)
+console.log(countNumberDuplicates(string_5) === 7)
+console.log(countNumberDuplicates(string_6) === 3)
