d30b655587f2f9c2a7ca66e9f255d62a1dcc23dd
Jack Hubert <jphubert93@gmail.com>
2022-02-10 09:55:56 -0700

    created a directory titled 'c' to start working through C# code challenges; created a 'convertNumberToString.cs' file to work through the challenge

---
+++ c/convertNumberToString.cs
@@ -0,0 +1,14 @@
+// CONVERT A NUMBER TO A STRING
+
+// We need a function that can transform a number into a string.
+// What ways of achieving this do you know?
+
+using System;
+
+public class Kata
+{
+  public static string NumberToString(int num)
+  {
+    // code here
+  }
+}
