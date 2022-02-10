d2c3fca7bc1d96e7d12ee34fe6369bc22563c7be
Jack Hubert <jphubert93@gmail.com>
2022-02-10 10:04:21 -0700

    created a Method 1 solution for the 'convertNumberToString' challenge

--- c/convertNumberToString.cs
+++ c/convertNumberToString.cs
@@ -3,12 +3,15 @@
 // We need a function that can transform a number into a string.
 // What ways of achieving this do you know?
 
+
+// Method 1 Solution
 using System;
 
 public class Kata
 {
   public static string NumberToString(int num)
   {
-    // code here
+    string msg = num.ToString();
+    return msg;
   }
 }
